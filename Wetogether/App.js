b0a746c9de4ed1d293ec0995cca56dfaa26555d3
Sandeep <sandeepmathew0982gmail.com>
2024-07-04 18:54:46 +0530

    Seperated screens

--- Wetogether/App.js
+++ Wetogether/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import AboutUs from './components/AboutUs';
 import CustomDrawerContent from './components/CustomDrawerContent';
-import HomeScreen from './components/HomeScreen';
-import ProfileScreen from './components/Profile';
 
+function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World</Text>
+      
+    </View>
+  );
+}
 const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator
-        initialRouteName="Home"
-        drawerContent={(props) => <CustomDrawerContent {...props} />}>
+            <Drawer.Navigator
+                initialRouteName="Home"
+                drawerContent={(props) => <CustomDrawerContent {...props} />}>
         <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Profile" component={ProfileScreen} />
         <Drawer.Screen name="About Us" component={AboutUs} />
-        
       </Drawer.Navigator>
       <StatusBar style="auto" />
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
